d42c5be3e08d41a1a447ef80c4e9faac85558b7b
Thomas <kindermann@kitconcept.com>
2021-04-12 11:37:53 +0200

    removed test and cleaned code

--- index.js
+++ index.js
@@ -22,23 +22,8 @@ app.get("/", (req, res) => {
   res.send("server is up and running");
 });
 
-// test number for /test
-let testNumber = 0;
-
 // client connects
 io.on("connection", (socket) => {
-  console.log("connected");
-
-  // initial response for test
-  io.emit("mutated", testNumber);
-
-  // increase function for test
-  socket.on("mutate", (number) => {
-    testNumber = testNumber + number;
-    console.log(testNumber);
-    io.emit("mutated", testNumber);
-  });
-
   // Avatar joined the floor
   socket.on("join", ({ name, color, x, y }) => {
     // add new Avatar in Avatar List
@@ -48,35 +33,28 @@ io.on("connection", (socket) => {
     io.emit("floorData", { avatars: getAvatars() });
 
     // remove If when test removed
-    if (getAvatar(socket.id)) {
-      console.log("users connected: ", getAvatars().length);
-    }
+
+    console.log("users connected: ", getAvatars().length);
   });
 
   // Update position on client avatar
   socket.on("update", ({ x, y }) => {
     // updates Avatar in List
     updateAvatar(socket.id, x, y);
+    //responds to client with updated avatar list
     io.emit("floorData", { avatars: getAvatars() });
-    // responds to clients with updated client positions
-    //io.emit("updateFloor", { updatedAvatar: getAvatar(socket.id) });
   });
 
   // user Disconnects
   socket.on("disconnect", () => {
-    // remove If when test removed
-    if (getAvatar(socket.id)) {
-      console.log(getAvatar(socket.id).name + " disconnected");
-    }
+    console.log(getAvatar(socket.id).name + " disconnected");
+
     removeAvatar(socket.id);
 
     //respond entire avatarlist to clients
     io.emit("floorData", { avatars: getAvatars() });
 
-    // remove If when test removed
-    if (getAvatars()) {
-      console.log("users connected: ", getAvatars().length);
-    }
+    console.log("users connected: ", getAvatars().length);
 
     //remove Avatar from avatar list
   });
